8f4a6a1f6a8111fe78d3f6705ec0989f6d8b48b1
Diego Perfecto <diego.perfect.7@gmail.com>
2024-03-14 17:39:04 -0600

    CSS changes & final details of the HTML

--- app.js
+++ app.js
@@ -20,12 +20,18 @@ for (var i = 0; i < textoIngresado.length; i++){
 
 if(textoIngresado != 0){
 document.getElementById("inputTextoEncriptado").value = textoEncriptado;
+document.getElementById("muñeco").setAttribute('style','display: none')    
+document.getElementById("msgError").setAttribute('style','display: none')
+document.getElementById("parrafo").setAttribute('style','display: none')    
+
+}else{
+    document.getElementById("muñeco").setAttribute('style','display: flex')    
+    document.getElementById("msgError").setAttribute('style','display: flex')    
+    document.getElementById("inputTextoEncriptado").value = '';        
+    document.getElementById("parrafo").setAttribute('style','display: flex')    
+
 }
 }
-
-
-
-
 
 function desencriptarTexto() {
 let textoIngresado = document.getElementById("textoIngresado").value;
